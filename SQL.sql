bd898bbb7007744bb6e5ea2faa2938cbc7572c0f
Serdar Şarbak <101435751+serdarsarbak@users.noreply.github.com>
2024-01-20 01:52:33 +0300

    SQL

--- SQL.sql
+++ SQL.sql
@@ -21,7 +21,7 @@ SELECT DISTINCT replacement_cost FROM film;
 SELECT COUNT (DISTINCT replacement_cost) from film;
 SELECT COUNT (*) FROM film WHERE title LIKE 'T%' AND rating = 'G'; 
 SELECT COUNT (*) FROM country WHERE LENGTH(country)=5;
-SELECT COUNT (*) FROM city WHERE city ='%r';
+SELECT COUNT (*) FROM city WHERE city ILIKE '%r';
 
 --HW5
 SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5;
