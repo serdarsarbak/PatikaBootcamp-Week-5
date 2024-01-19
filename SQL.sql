4fbf7f540f0af19a01bb35c2d6a984566456811a
Serdar Şarbak <101435751+serdarsarbak@users.noreply.github.com>
2024-01-20 01:52:59 +0300

    SQL

--- SQL.sql
+++ SQL.sql
@@ -43,10 +43,5 @@ SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC
 
 
 
-       4. city tablosunda bulunan şehir verilerini 
-  country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
-
-
-
 
 
