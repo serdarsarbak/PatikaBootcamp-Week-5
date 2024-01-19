bfa0d5385f57dd67ecfbeb31c660f34bcf960c9f
Serdar Şarbak <101435751+serdarsarbak@users.noreply.github.com>
2024-01-19 05:37:52 +0300

    SQL

--- SQL.sql
+++ SQL.sql
@@ -10,3 +10,10 @@ SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 SELECT first_name, last_name FROM actor WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
 
+--HM3
+SELECT * FROM country WHERE country LIKE 'A%a';
+SELECT * FROM country WHERE LENGTH(country) >= 6 AND country LIKE '%n';
+SELECT * FROM film WHERE title ILIKE '%t%';
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+
+
