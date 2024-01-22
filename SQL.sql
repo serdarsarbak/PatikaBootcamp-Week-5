7a4ad8c1f61d6afda98b7eee3fa4be0536aab407
Serdar Şarbak <101435751+serdarsarbak@users.noreply.github.com>
2024-01-22 23:55:05 +0300

    SQL

--- SQL.sql
+++ SQL.sql
@@ -123,6 +123,17 @@ SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
 
 SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer FULL JOIN rental ON customer.customer_id = rental.customer_id;
 
+--HW11
+(SELECT first_name FROM actor) UNION (SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor) INTERSECT (SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor) EXCEPT (SELECT first_name FROM customer);
+
+(SELECT first_name FROM actor) UNION ALL (SELECT first_name FROM customer);
+(SELECT first_name FROM actor) INTERSECT ALL (SELECT first_name FROM customer);
+(SELECT first_name FROM actor) EXCEPT ALL (SELECT first_name FROM customer);
+
 
 
 
