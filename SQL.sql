93f085a209dc78a702ecb3c655f284af085a314a
Serdar Şarbak <101435751+serdarsarbak@users.noreply.github.com>
2024-01-22 22:11:02 +0300

    SQL

--- SQL.sql
+++ SQL.sql
@@ -106,6 +106,17 @@ DELETE FROM employee WHERE id = 5;
 DELETE FROM employee WHERE id = 7;
 DELETE FROM employee WHERE id = 12;
 
+--HW9
+
+SELECT city, country FROM country
+  INNER JOIN city ON city.country_id = country.country_id;
+
+SELECT payment_id, first_name, last_name FROM customer
+  INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+SELECT rental_id, first_name, last_name FROM customer
+  INNER JOIN rental ON rental.customer_id = customer.customer_id;
+
 
 
 
